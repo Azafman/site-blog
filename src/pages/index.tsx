54f9189ce80bfe2d9505bdb8bf818b471ff90999
Azafman <0000880867@senaimgaluno.com.br>
2026-01-01 22:43:41 -0300

    📦 build: instalando shadcnui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="text-4xl font-bold text-red-500">Welcome to Next.js!</h1>
+      <Button variant={"destructive"}>
+        Reload Page
+      </Button>
     </main>
   );
 }
